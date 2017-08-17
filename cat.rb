0cf6376f3ad760dfd5015b2b601ddc736ab5a58e
Irene DeVera <irenneeyyy@gmail.com>
2017-08-17 09:48:47 -0700

    Add second cat to cat.rb

--- cat.rb
+++ cat.rb
@@ -1,6 +1,14 @@
 class Cat
   def initialize(name)
+    @name = name
+  end
+  def display_name
+    return @name
   end
 end
 
 octocat = Cat.new("Octocat")
+maxi = Cat.new("Maxwell")
+
+puts octocat.display_name
+puts maxi.display_name
